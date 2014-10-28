90b0c19b4d01d653ddf2d60be7233e78700a458f
krk <keremkat@gmail.com>
2014-10-28 12:44:53 +0200

    Humanoid class with a method containing a dynamic local variable created.

---
+++ CodeContractsRosyln/Humanoid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeContractsRosyln
+{
+    public class Humanoid
+    {
+        /* This method has a dynamic local variable preventing it to be rewritten 
+           by Code Contracts rewriter if the compiler is Roslyn. */
+        public void Bark()
+        {
+            /* Method does not need to have a Contracts clause. */
+
+            dynamic c = 1;
+
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
